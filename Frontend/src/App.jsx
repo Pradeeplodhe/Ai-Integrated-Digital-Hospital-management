c881e1f8b79d7cb5e327a134fcc151396062951b
Pradeep lodhi <lodhipradeep948@gmail.com>
2025-12-02 12:07:57 +0530

    resetup all the project

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import PatientApp from './Patient/PatientApp'
-
 function App() {
  
   return (
  <>
-  <PatientApp/>
+ <h1>helloo</h1>
  </>
   )
 }
